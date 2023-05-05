3a19e74572da97ce226e91404e23407c3f2a4d49
Mukesh Verma <mukeshverma643@gmail.com>
2023-05-05 17:59:32 +0530

    Finishing the Theme Toggler

--- src/Colors.js
+++ src/Colors.js
@@ -1,15 +1,15 @@
-const AppTheme = () =>{
+const AppTheme = {
     light:{
-        textColor:'#000';
-        backgroundColor:'#FFF';
-        border:'2px Solid #000;'
+        textColor:'#000',
+        backgroundColor:'#FFF',
+        border:'2px Solid #000',
     },
 
     dark:{
-        textColor:'#FFF';
-        backgroundColor:'#333';
-        border:'2px Dashed #000'
+        textColor:'#FFF',
+        backgroundColor:'#333',
+        border:'2px Dashed #000',
     }
-}
+};
 
 export default AppTheme;
---
+++ src/Components/HeroSection.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import ThemeContext from "../Context/ThemeContext";
+import AppTheme from "../Colors";
+
+const HeroSection = () => {
+  const theme = useContext(ThemeContext)[0];
+  const currentTheme = AppTheme[theme];
+
+  return (
+    <div style={{
+        padding:"1rem",
+        backgroundColor:`${currentTheme.backgroundColor}`,
+        color:`${currentTheme.textColor}`
+    }}>
+      <h1>Context API Theme Toggler</h1>
+      <p>This is Description</p>
+      <button
+        style={{
+          backgroundColor: "#26ae60",
+          padding: "10px 150px",
+          fontSize: "20px",
+          color: "white",
+          border: `${currentTheme.border}`
+        }}
+      >Toggle</button>
+    </div>
+  );
+};
+
+export default HeroSection;
