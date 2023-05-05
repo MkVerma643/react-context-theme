ce076acb6fb88ef0c99bda7a2774e3fb75f2afc8
Mukesh Verma <mukeshverma643@gmail.com>
2023-05-05 11:26:29 +0530

    Adding Toggle Button for Theme

---
+++ src/Colors.js
@@ -0,0 +1,15 @@
+const AppTheme = () =>{
+    light:{
+        textColor:'#000';
+        backgroundColor:'#FFF';
+        border:'2px Solid #000;'
+    },
+
+    dark:{
+        textColor:'#FFF';
+        backgroundColor:'#333';
+        border:'2px Dashed #000'
+    }
+}
+
+export default AppTheme;
